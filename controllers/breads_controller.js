9a640a659dee0d5f833a9736ddd8a87e0504371c
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-02-12 11:48:44 -0800

    create and new routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -8,6 +8,11 @@ bread_router.get('/', (req, res) => {
   // res.send(Bread)
 })
 
+// NEW
+bread_router.get('/new', (req, res) => {
+    res.render('new')
+})
+
 //SHOW
 bread_router.get('/:arrayIndex', (req, res) => {
     if (bread_data[req.params.arrayIndex]) {
@@ -19,4 +24,25 @@ bread_router.get('/:arrayIndex', (req, res) => {
     }
 })
 
+//CREATE
+// CREATE
+bread_router.post('/', (req, res) => {
+    //why does this call for an exclamation mark? v
+    if (!req.body.image) {
+      req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    }
+    if(req.body.hasGluten === 'on') {
+      req.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    bread_data.push(req.body)
+    res.redirect('/breads')
+  })
+  
+
 module.exports = bread_router
+
+//if (!req.body.image) {
+    //req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+//}
