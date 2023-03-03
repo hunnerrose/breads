2d1b6feeb9dd5cbfe68a7d3fc941d8e4968b9eed
hunnerrose <114459778+hunnerrose@users.noreply.github.com>
2023-03-02 17:53:43 -0800

    added missing key in the new page

--- views/new.jsx
+++ views/new.jsx
@@ -25,7 +25,7 @@ function New ({bakers}) {
           <select name="baker" id="baker">
             {bakers.map((baker) => {
               return(
-                <option value={baker.id}>{baker.name}</option>
+                <option value={baker.id} key={baker.id}>{baker.name}</option>
               )
             })}
           </select>
